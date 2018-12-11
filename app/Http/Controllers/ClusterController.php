598d086b35813c98c3f22873b549ec99857e2645
Hainan <402945786@qq.com>
2018-12-11 23:20:33 +0800

    add purity algorithm

--- app/Http/Controllers/ClusterController.php
+++ app/Http/Controllers/ClusterController.php
@@ -14,14 +14,26 @@ class ClusterController extends Controller
 {
     const DMS = 22; // 数据维度
 
+    const KINDS = [
+        'Leptodactylidae',
+        'Dendrobatidae',
+        'Hylidae',
+        'Bufonidae'
+    ];
+
     public function index() {
         // 设置聚类组数
         $k = 4;
         // 加载数据
         $data = self::loadData();
         //聚类
-        list($clusterResults, $centerPoints) = self::kMeans($data, $k);
-        dd($clusterResults, $centerPoints);
+        //list($clusterResults, $centerPoints, $clusterDistribution) = self::kMeans($data, $k);
+        $clusterDistribution = self::kMeans($data, $k);
+        // purity评价
+        $purity = self::purity($clusterDistribution);
+        // f-score评价
+        echo $purity;
+        dd($clusterDistribution);
     }
 
     public static function loadData() {
@@ -72,6 +84,8 @@ class ClusterController extends Controller
         $changeFlag = true;
         // 初始化聚类结果数组，并存储距中心点距离
         $clusterResults = [];
+        // 初始化聚类分布数组
+        $clusterDistribution = [];
         $cct = 0;
         // 聚类
         while($changeFlag) {
@@ -80,6 +94,7 @@ class ClusterController extends Controller
             $changeFlag = false;
             // 初始化聚类结果数组，并存储距中心点距离
             $clusterResults = [];
+            $clusterDistribution = [];
             foreach($data as $datum) {
                 $minDist = 99999;
                 $minIndex = -1;
@@ -93,10 +108,12 @@ class ClusterController extends Controller
                     }
                 }
                 // 把这个点归类到那个中心点
-                $clusterResults[$minIndex][] = [
-                    'point' => $datum,
-                    'dist' => $minDist
-                ];
+//                $clusterResults[$minIndex][] = [
+//                    'point' => $datum,
+//                    'dist' => $minDist
+//                ];
+                // 统计聚类信息
+                isset($clusterDistribution[$minIndex][$datum[22]]) ? $clusterDistribution[$minIndex][$datum[22]]++ : ($clusterDistribution[$minIndex][$datum[22]] = 1);
             }
 
             // 重新计算中心点
@@ -119,8 +136,39 @@ class ClusterController extends Controller
             }
         }
 
+        // 数据处理
+        foreach ($clusterDistribution as $ak => &$av) {
+            foreach (self::KINDS as $kv) {
+                if(!isset($av[$kv])) {
+                    $av[$kv] = 0;
+                }
+            }
+        }
+        unset($av);
+
         // 数据返回，中心点位置和聚类结果
-        return [$clusterResults, $centerPoints];
+        //return [$clusterResults, $centerPoints, $clusterDistribution];
+        return $clusterDistribution;
+    }
+
+    public static function purity($table) {
+        $msg = "<table border='1'><tr><td>Group</td>";
+        foreach(self::KINDS as $kv) {
+            $msg .= "<td>" . $kv . "</td>";
+            $msg .= "<td>" . $kv . "-Purity</td>";
+        }
+        $msg .= '</tr>';
+        foreach ($table as $ak => $av) {
+            $msg .= '<tr><td>' . $ak . "</td>";
+            $sum = array_sum($av);
+            foreach(self::KINDS as $kv) {
+                $avValue = $av[$kv];
+                $msg .= "<td>" . $avValue . "</td>";
+                $msg .= "<td>" . $avValue/$sum . "</td>";
+            }
+        }
+        $msg .= '</table>';
+        return $msg;
     }
 }
 
