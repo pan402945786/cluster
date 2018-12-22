58cd7a2e74c7079a08bd4d026a770c1da06bbe01
潘海楠 <panhainan@panhainandeMacBook-Pro.local>
2018-12-22 20:34:32 +0800

    fix bug

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -192,7 +192,7 @@
 <!--<script src="revolution/js/jquery.themepunch.tools.min.js"></script>-->
 <!--<script src="revolution/js/jquery.themepunch.revolution.min.js"></script>-->
 <!---->
-<!--<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->-->
+<!--<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
 <!--<script src="revolution/js/extensions/revolution.extension.actions.min.js"></script>-->
 <!--<script src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>-->
 <!--<script src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>-->
@@ -202,7 +202,7 @@
 <!--<script src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>-->
 <!--<script src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>-->
 <!--<script src="revolution/js/extensions/revolution.extension.video.min.js"></script>-->
-<!--<!-- revolution custom -->-->
+<!--<!-- revolution custom -->
 <!--<script src="revolution/js/revolution-custom.js"></script>-->
 
 <!-- custom -->
